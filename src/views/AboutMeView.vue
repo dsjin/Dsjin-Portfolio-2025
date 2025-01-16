946da980b98b374382baede461052eebc56fff28
dsjin <dsjinj@gmail.com>
2025-01-16 16:35:46 +0900

    fix(aboutme): fixed responsive

--- src/views/AboutMeView.vue
+++ src/views/AboutMeView.vue
@@ -7,10 +7,10 @@ import '@lottiefiles/lottie-player'
     <div class="flex flex-col justify-start items-center h-full px-2">
       <h1 class="text-3xl mb-5 lg:mb-0">About Me</h1>
       <div
-        class="flex flex-col lg:flex-row justify-between items-center h-full w-full"
+        class="flex flex-col lg:flex-row lg:justify-between justify-stretch items-center h-full w-full"
       >
         <div
-          class="flex flex-col justify-center h-full w-1/2 lg:mr-4 mb-5 lg:mb-0 order-2 lg:order-1"
+          class="flex flex-col justify-center lg:h-full lg:w-1/2 w-full lg:mr-4 mb-5 lg:mb-0 order-2 lg:order-1"
         >
           <p class="text-lg mb-10">
             &nbsp;&nbsp;&nbsp;&nbsp;An ordinary web full-stack developer who
@@ -46,7 +46,7 @@ import '@lottiefiles/lottie-player'
           </div>
         </div>
         <div
-          class="flex justify-center h-full w-1/2 ld:ml-4 mb-5 lg:mb-0 order-1 lg:order-2"
+          class="flex justify-center lg:h-full w-1/2 ld:ml-4 mb-5 lg:mb-0 order-1 lg:order-2"
         >
           <lottie-player
             src="https://assets2.lottiefiles.com/packages/lf20_o1JRaPxx1E.json"
