b5ec987e2a7a73b2be6c40bc30c65e23f2f3c86e
dsjin <dsjinj@gmail.com>
2025-04-03 16:55:08 +0900

    chore: changed the work item

--- src/assets/data/workItems.ts
+++ src/assets/data/workItems.ts
@@ -6,12 +6,9 @@ export const workItems: WorkItem[] = [
     id: uuid(),
     title: 'Todo List',
     description:
-      "A web application that allow you to manage a group of tasks, manage a task. All informtion is stored in the cloud ( Supabase server )",
-    inProcess: true,
-    techStacks: [
-      'Vue.JS',
-      'Supabase',
-    ],
+      'A web application that allow you to manage a group of tasks, manage a task. All informtion is stored in the cloud ( Supabase server )',
+    inProcess: false,
+    techStacks: ['Vue.JS', 'Supabase'],
     link: 'https://todo.dsjin.co',
     thumbnailPath: '',
     year: '2024',
@@ -22,11 +19,7 @@ export const workItems: WorkItem[] = [
     description:
       "A web application that scans qrcode with your device's cameras. It will save the scan result in IndexedDB for history. It is also able to generate QRcode and save it to your device.",
     inProcess: false,
-    techStacks: [
-      'Vue.JS',
-      'IndexedDB',
-      'Media Source Web API',
-    ],
+    techStacks: ['Vue.JS', 'IndexedDB', 'Media Source Web API'],
     link: 'https://qrcode.dsjin.co',
     thumbnailPath: '',
     year: '2023',
