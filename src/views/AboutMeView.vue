4f1cf57b596cff25c1fe18484315ba5e7aec810f
Thatchakon Jom-ud <dsjinj@gmail.com>
2026-01-28 14:43:12 +0900

    feat: added skills and experiences (#1)
    * doc: fixed misspelling
    
    * Update README.md
    
    * Update README.md
    
    * feat: added nav item
    
    * feat: added skills and experiences

--- src/views/AboutMeView.vue
+++ src/views/AboutMeView.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-import { Button } from 'primevue'
+import { Button, Panel, Chip } from 'primevue'
+import { experiences } from '../assets/data/experienceItems'
+import { skills } from '../assets/data/skillItems'
 import '@lottiefiles/lottie-player'
 </script>
 <template>
-  <div id="AboutMe" class="p-3 text-white">
-    <div class="flex flex-col justify-start items-center h-full px-2">
+  <div id="AboutMe" class="p-4 text-white lg:p-14">
+    <div class="flex flex-col justify-start items-center h-full px-2 mb-5 lg:mb-0">
       <h1 class="text-3xl mb-5 lg:mb-0">About Me</h1>
-      <div
-        class="flex flex-col lg:flex-row lg:justify-between justify-stretch items-center h-full w-full"
-      >
-        <div
-          class="flex flex-col justify-center lg:h-full lg:w-1/2 w-full lg:mr-4 mb-5 lg:mb-0 order-2 lg:order-1"
-        >
+      <div class="flex flex-col lg:flex-row lg:justify-between justify-stretch items-center h-full w-full">
+        <div class="flex flex-col justify-center lg:h-full lg:w-1/2 w-full lg:mr-4 mb-5 lg:mb-0 order-2 lg:order-1">
           <p class="text-lg mb-10">
             &nbsp;&nbsp;&nbsp;&nbsp;An ordinary web full-stack developer who
             currently lives in Japan (and a DOG person 🐕). I primarily focus on
@@ -20,49 +18,47 @@ import '@lottiefiles/lottie-player'
             via these services below.
           </p>
           <div class="w-full flex justify-center">
-            <Button
-              as="a"
-              class="mr-2"
-              icon="pi pi-twitter"
-              variant="outlined"
-              href="https://twitter.com/dsjinj"
-              target="_blank"
-            />
-            <Button
-              as="a"
-              class="mr-2"
-              icon="pi pi-github"
-              variant="outlined"
-              href="https://github.com/dsjin"
-              target="_blank"
-            />
-            <Button
-              as="a"
-              icon="pi pi-linkedin"
-              variant="outlined"
-              href="https://www.linkedin.com/in/thatchakonj/"
-              target="_blank"
-            />
+            <Button as="a" class="mr-2" icon="pi pi-twitter" variant="outlined" href="https://twitter.com/dsjinj"
+              target="_blank" />
+            <Button as="a" class="mr-2" icon="pi pi-github" variant="outlined" href="https://github.com/dsjin"
+              target="_blank" />
+            <Button as="a" icon="pi pi-linkedin" variant="outlined" href="https://www.linkedin.com/in/thatchakonj/"
+              target="_blank" />
           </div>
         </div>
-        <div
-          class="flex justify-center lg:h-full w-1/2 ld:ml-4 mb-5 lg:mb-0 order-1 lg:order-2"
-        >
-          <lottie-player
-            src="https://assets2.lottiefiles.com/packages/lf20_o1JRaPxx1E.json"
-            background="transparent"
-            speed="1"
-            class="w-3/4"
-            loop
-            autoplay
-          />
+        <div class="flex justify-center lg:h-full w-1/2 ld:ml-4 mb-5 lg:mb-0 order-1 lg:order-2">
+          <lottie-player src="https://assets2.lottiefiles.com/packages/lf20_o1JRaPxx1E.json" background="transparent"
+            speed="1" class="w-3/4" loop autoplay />
         </div>
       </div>
+    </div>
+    <div class="flex flex-col items-center h-full px-2 mb-5">
+      <h1 class="text-3xl mb-10">
+        Skills / Tech Stacks
+      </h1>
+      <div class="flex flex-wrap justify-center gap-4 mb-4 lg:w-3/4">
+        <Chip
+          v-for="item in skills"
+          :key="item.id"
+          :label="item.name"
+        />
+      </div>
+    </div>
+    <div class="flex flex-col items-center h-full px-2">
+      <h1 class="text-3xl mb-10">
+        Experiences
+      </h1>
+      <Panel :key="item.id" v-for="item in experiences" :header="item.title" class="mb-5 w-full lg:w-3/4">
+        <template #icons>
+          {{ item.year }}
+        </template>
+        <div v-html="item.template"></div>
+      </Panel>
     </div>
   </div>
 </template>
 <style lang="scss">
 #AboutMe {
-  @include common.onepage-wrapper;
+  @include common.longpage-wrapper;
 }
 </style>
